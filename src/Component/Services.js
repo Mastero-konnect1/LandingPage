d2d9c96709bdc8af651faef6114dc6f58feb9f69
Mastero-konnect1 <masterokonnect@gmail.com>
2024-07-19 08:32:43 +0530

    updated

--- src/Component/Services.js
+++ src/Component/Services.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import logo from './Services.png'
+// src/components/Services.js
 
-function Services() {
+import React from 'react';
+import './Services.css';
+import serviceImage from './Services.png';
+
+const Services = () => {
   return (
-    <div className='App3'>
-      <div className="image">
-        <img src={logo} alt="Sample" />
+    <div className="services-wrapper">
+      <div className="services-container">
+        <img src={serviceImage} alt="Service" className="service-image" />
       </div>
     </div>
   );
-}
+};
 
 export default Services;
