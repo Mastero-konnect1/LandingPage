2123fdeed67dc541ea5c2edcc67cd70d71e0b136
Mastero-konnect1 <masterokonnect@gmail.com>
2024-07-19 12:40:08 +0530

    updated

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Navbar.css';
 import logo from './image.png'; 
 
@@ -6,20 +6,35 @@ const handleButtonClick = () => {
   window.location.href = '#Community'; 
 };
 
-const Navbar = () => {
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () =>{
+    this.setState({clicked:
+      !this.state.clicked
+    })
+  }
+  render() {
+
   return (
     <div className="navbar">
       <div className="navbar-left">
         <img src={logo} alt="Logo" className="logo" />
       </div>
-      <div className="navbar-right">
+      <div id="navbar-right" className={this.state.clicked ? 
+        "#navbar-right nav-link" : "#navbar-right"}>
+
         <a href="#Home" className="nav-link" id='H'>Home</a>
         <a href="#Services" className="nav-link" id='H'>Products</a>
         <a href="#Contact" className="nav-link" id='H'>Contact</a>
         <button className="nav-button"  onClick={handleButtonClick}>Join Community</button>
       </div>
+      
+      <div id='mobile' onClick={this.handleClick}>
+        <i id='bar' className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
     </div>
   );
 };
+}
 
 export default Navbar;
